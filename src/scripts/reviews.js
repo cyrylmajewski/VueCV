e48bc75cd5a39dc1fa33e0c8320ecb48c8f79dfb
CrackThis <prm1078life@gmail.com>
2020-11-09 17:19:46 +0100

    homework_2 responsive

---
+++ src/scripts/reviews.js
@@ -0,0 +1,48 @@
+import Vue from "vue";
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
+import 'swiper/swiper-bundle.css';
+
+new Vue({
+    el: "#slider-component",
+    template: "#slider-container",
+    components: {
+      Swiper, SwiperSlide
+    },
+    data() {
+        return {
+            reviews: [],
+            sliderOptions: {
+                slidesPerView: 2,
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1
+                    }
+                }
+            }
+        }
+    },
+    methods: {
+        requireImgToArray(data) {
+            return data.map((item) => {
+                const requiredImage = require(`../images/content/${item.pic}`).default;
+                item.pic = requiredImage;
+                return item
+            });
+        },
+        slide(direction) {
+            const slider = this.$refs["slider"].$swiper;
+            switch(direction) {
+                case "next" :
+                    slider.slideNext();
+                    break;
+                case "prev" :
+                    slider.slidePrev();
+                    break;
+            }
+        }
+    },
+    created() {
+        const data = require("../data/reviews.json");
+        this.reviews = this.requireImgToArray(data);
+    }
+});
